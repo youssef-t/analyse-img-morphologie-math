b97a434e37f131fdfd25d78f863287bc834d799f
youssef-t <yousseftaleb1999@gmail.com>
2021-01-16 13:38:07 +0100

    Prise en compte des images binaires pour l'addition et la soustraction

--- functions.py
+++ functions.py
@@ -2,12 +2,12 @@
 import numpy as np
 
 '''
-Remarque: toutes les fonctions n'acceptent que des images binaires 
+Remarque: toutes les fonctions n'acceptent que des images avec des niveaux de gris ou binaires
 '''
 def seuil_image(img, seuil):
     x_max = len(img)
     y_max = len(img[0])
-    img_apres_seuillage = np.zeros((x_max, y_max))
+    img_apres_seuillage = np.zeros((x_max, y_max), dtype=int)
     for i in range(0, x_max):
         for j in range(0, y_max):
             if img[i][j] > seuil:
@@ -17,30 +17,47 @@ def seuil_image(img, seuil):
     return img_apres_seuillage
 
 
-def addition_2_images(img1, img2):
+def addition_2_images(img1, img2, is_binaire=True):
     x_max = len(img1)
     y_max = len(img2)
-    img_apres_addition = np.zeros((x_max, y_max))
+    img_apres_addition = np.zeros((x_max, y_max), dtype=int)
     for i in range(0, x_max):
         for j in range(0, y_max):
-            img_apres_addition[i][j] = img1[i][j] + img2[i][j]
+            valeur_pixel = img1[i][j] + img2[i][j]
+            # si l'image n'est pas binaire
+            if not is_binaire:
+                if valeur_pixel > 255:
+                    img_apres_addition[i][j] = 255
+                else:
+                    img_apres_addition[i][j] = valeur_pixel
+            #si l'image est binaire
+            else:
+                if valeur_pixel > 1:
+                    img_apres_addition[i][j] = 1
+                else:
+                    img_apres_addition[i][j] = valeur_pixel
+
     return img_apres_addition
 
-
+#Remarque: on n'a pas besoin de spécifier si l'image est binaire ou en niveau de gris
 def soustraction_2_images(img1, img2):
     x_max = len(img1)
     y_max = len(img1[0])
-    img_apres_soustraction = np.zeros((x_max, y_max))
+    img_apres_soustraction = np.zeros((x_max, y_max), dtype=int)
     for i in range(0, x_max):
         for j in range(0, y_max):
-            img_apres_soustraction[i][j] = img1[i][j] - img2[i][j]
+            valeur_pixel = img1[i][j] - img2[i][j]
+            if valeur_pixel < 0:
+                img_apres_soustraction[i][j] = 0
+            else:
+                img_apres_soustraction[i][j] = valeur_pixel
     return img_apres_soustraction
 
 
 def erosion_image_binaire(img):
     x_max = len(img)
     y_max = len(img[0])
-    img_apres_erosion = np.zeros((x_max, y_max))
+    img_apres_erosion = np.zeros((x_max, y_max), dtype=int)
     # parcourir l'image
     for i in range(1, x_max-1):
         for j in range(1, y_max-1):
@@ -58,7 +75,7 @@ def erosion_image_binaire(img):
 def dilatation_image_binaire(img):
     x_max = len(img)
     y_max = len(img[0])
-    img_apres_dilatation = np.zeros((x_max, y_max))
+    img_apres_dilatation = np.zeros((x_max, y_max), dtype=int)
     # parcourir l'image
     for i in range(1, x_max-1):
         for j in range(1, y_max-1):
@@ -85,18 +102,30 @@ def fermeture_img(img):
     return img_apres_ouverture
 
 
-def amincissement_img(img):
-    '''
-    :param img:
-    :return:
-    '''
+'''
+on réalise l'amincissement et l'epaississement avec une configuration qu'on donne dans les paramètres
+la configuration correspond à un élément structurant
+la valeur 2 correspond à une valeur quelconque 
+Example d'élément structant:
+    element_structurant = [[0 1 0]
+                           [2 1 2]
+                           [0 1 0]]
+'''
 
 
-def epaississement_img(img):
-    '''
+def amincissement_img(img, element_structurant):
+    x_max = len(img)
+    y_max = len(img[0])
+    img_apres_amincissement = np.zeros((x_max, y_max), dtype=int)
 
-    :param img:
-    :return:
-    '''
+    return img_apres_amincissement
+
+
+def epaississement_img(img, element_structurant):
+    x_max = len(img)
+    y_max = len(img[0])
+    img_apres_epaississement = np.zeros((x_max, y_max), dtype=int)
+
+    return img_apres_epaississement
 
 
