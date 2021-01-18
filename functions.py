7600b4014aa589341d96d8d5cf87f0cf45063117
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2021-01-18 10:10:38 +0100

    [REFACTOR] Refactored functions.

--- functions.py
+++ functions.py
@@ -21,6 +21,7 @@ def seuil_image(img, seuil):
 def addition_2_images(img1, img2, is_binaire=True):
     x_max = len(img1)
     y_max = len(img2)
+    y_max = len(img2)
     img_apres_addition = np.zeros((x_max, y_max), dtype=int)
 
     if is_binaire:
@@ -80,29 +81,28 @@ def erosion_image_binaire(img, rayon_element_structurant=1):
     # parcourir l'image en prenant en compte la taille de l'élément structurant
     for i in range(rayon_element_structurant, x_max - rayon_element_structurant):
         for j in range(rayon_element_structurant, y_max - rayon_element_structurant):
-            #pour ne pas faire les calculs pour les pixels de valeur 0
+            # pour ne pas faire les calculs pour les pixels de valeur 0
             if img[i][j] == 1:
                 s = 0
                 # element structurant
                 for ligne_elem_struct in range(-rayon_element_structurant, rayon_element_structurant + 1):
                     for col_elem_struct in range(-rayon_element_structurant, rayon_element_structurant + 1):
                         s += img[i + ligne_elem_struct][j + col_elem_struct]
-                #rayon 1 => 3*3 = (2*1 +1)^2; rayon 2 => 5*5 = (2*2 +1)^2; rayon 3 => (2*3+1)^2
-                #rayon n => (2*n +1)^2 éléments dans la matrice
-                if s != (2*rayon_element_structurant + 1)**2:
+                # rayon 1 => 3*3 = (2*1 +1)^2; rayon 2 => 5*5 = (2*2 +1)^2; rayon 3 => (2*3+1)^2
+                # rayon n => (2*n +1)^2 éléments dans la matrice
+                if s != (2 * rayon_element_structurant + 1) ** 2:
                     img_apres_erosion[i][j] = 0
 
-    #on met les pixels non parcourus à 0
-    #bords de l'image
+    # on met les pixels non parcourus à 0
+    # bords de l'image
     for i in range(0, x_max):
         for j in range(0, rayon_element_structurant):
             img_apres_erosion[i][j] = 0
-            img_apres_erosion[i][y_max-j-1] = 0
+            img_apres_erosion[i][y_max - j - 1] = 0
     for j in range(0, y_max):
         for i in range(0, rayon_element_structurant):
             img_apres_erosion[i][j] = 0
-            img_apres_erosion[x_max-i-1][j] = 0
-
+            img_apres_erosion[x_max - i - 1][j] = 0
 
     return img_apres_erosion
 
@@ -156,6 +156,7 @@ ELEMENT_STRUCTURANT_PAR_DEFAUT = [[1, 1, 1],
                                   [1, 1, 1],
                                   [1, 1, 1]]
 
+
 def amincissement_img(img, element_structurant=ELEMENT_STRUCTURANT_PAR_DEFAUT):
     x_max = len(img)
     y_max = len(img[0])
@@ -173,11 +174,11 @@ def amincissement_img(img, element_structurant=ELEMENT_STRUCTURANT_PAR_DEFAUT):
                 # break_boucle = False
                 for ligne_elem_struct in range(-1, 2):
                     for col_elem_struct in range(-1, 2):
-                        if element_structurant[ligne_elem_struct+1][col_elem_struct+1] == 2:
+                        if element_structurant[ligne_elem_struct + 1][col_elem_struct + 1] == 2:
                             nbr_pixels_identiques += 1
                         else:
                             differance = img[i + ligne_elem_struct][j + col_elem_struct] \
-                                         - element_structurant[ligne_elem_struct+1][col_elem_struct+1]
+                                         - element_structurant[ligne_elem_struct + 1][col_elem_struct + 1]
                             if differance == 0:
                                 nbr_pixels_identiques += 1
                                 '''
@@ -194,6 +195,7 @@ def amincissement_img(img, element_structurant=ELEMENT_STRUCTURANT_PAR_DEFAUT):
                     img_apres_amincissement[i][j] = 0
 
     return img_apres_amincissement
+
 
 # Remarque: avec l'élément structurant par défaut, aucune modification sera faite
 def epaississement_img(img, element_structurant=ELEMENT_STRUCTURANT_PAR_DEFAUT):
@@ -213,11 +215,11 @@ def epaississement_img(img, element_structurant=ELEMENT_STRUCTURANT_PAR_DEFAUT):
                 # break_boucle = False
                 for ligne_elem_struct in range(-1, 2):
                     for col_elem_struct in range(-1, 2):
-                        if element_structurant[ligne_elem_struct+1][col_elem_struct+1] == 2:
+                        if element_structurant[ligne_elem_struct + 1][col_elem_struct + 1] == 2:
                             nbr_pixels_identiques += 1
                         else:
                             differance = img[i + ligne_elem_struct][j + col_elem_struct] \
-                                         - element_structurant[ligne_elem_struct+1][col_elem_struct+1]
+                                         - element_structurant[ligne_elem_struct + 1][col_elem_struct + 1]
                             if differance == 0:
                                 nbr_pixels_identiques += 1
                                 '''
@@ -242,8 +244,8 @@ def squelette_lantuejoul(img, iteration_lantejoul=30):
     img_squelette_lantuejoul = np.zeros((x_max, y_max), dtype=int)
 
     img_full_zeros = np.zeros((x_max, y_max), dtype=int)
-# formule simplifiée du théorème de Lantuejoul: on commence de 1
-    for n in range(1, iteration_lantejoul+1):
+    # formule simplifiée du théorème de Lantuejoul: on commence de 1
+    for n in range(1, iteration_lantejoul + 1):
         img_erodee_rayon_n = erosion_image_binaire(img, n)
         if (img_erodee_rayon_n == img_full_zeros).all():
             break
@@ -256,18 +258,17 @@ def squelette_lantuejoul(img, iteration_lantejoul=30):
 
 
 def squelette_amincissement_homotopique(img):
-
     # copier l'image dans des variables locales
     img_squelette_amincissement_homotopique_post = np.array(img, dtype=int)
     img_squelette_amincissement_homotopique_pre = np.array(img, dtype=int)
-    #la valeur 2 correspond à une valeur quelconque
+    # la valeur 2 correspond à une valeur quelconque
     element_structurant = [[2, 0, 2],
                            [1, 1, 0],
                            [1, 1, 2]]
 
-    #calculer le squelette jusqu'à l'idempotance
-    #la boucle "do...while(condition)" n'existe pas en Python
-    #on utilise alors la syntaxe suivante: while True ... if(condition): break
+    # calculer le squelette jusqu'à l'idempotance
+    # la boucle "do...while(condition)" n'existe pas en Python
+    # on utilise alors la syntaxe suivante: while True ... if(condition): break
     compteur = 0
     while True:
         img_squelette_amincissement_homotopique_post = amincissement_img(img_squelette_amincissement_homotopique_post,
@@ -276,12 +277,12 @@ def squelette_amincissement_homotopique(img):
         compteur += 1
         print(f'Amincissement homotopique - boucle: {compteur}')
 
-        #vérifier l'idempotance
+        # vérifier l'idempotance
         if (img_squelette_amincissement_homotopique_pre == img_squelette_amincissement_homotopique_post).all() or \
                 compteur > 500:
             break
 
-        #copie par valeur
+        # copie par valeur
         img_squelette_amincissement_homotopique_pre = np.copy(img_squelette_amincissement_homotopique_post)
 
     return img_squelette_amincissement_homotopique_post
